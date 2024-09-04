e91586691c9602e8cfda611901b2c81a887a951a
Marcelo de Sousa Silva <sousams93@outlook.com>
2024-09-04 18:15:27 -0300

    filter

--- src/test/java/com/amigoscode/examples/GettingStarted.java
+++ src/test/java/com/amigoscode/examples/GettingStarted.java
@@ -18,12 +18,31 @@ public class GettingStarted {
         // 1. Find people aged less or equal 18
         // 2. Then change implementation to find first 10 people
         List<Person> people = MockData.getPeople();
+        List<Person> newPeopleList = new ArrayList<>();
+        int limit = 10;
+        int age = 18;
+        int counter = 0;
 
-        people.stream().filter(person -> person.getAge() <= 18).forEach(System.out::println);
+        for(Person person : people){
+            if(person.getAge() <= age){
+                newPeopleList.add(person);
+                counter++;
+                if(counter == limit){
+                    break;
+                }
+            }
+        }
+        newPeopleList.forEach(System.out::println);
+
     }
 
     @Test
     public void declarativeApproachUsingStreams() throws Exception {
         List<Person> people = MockData.getPeople();
+        List<Person> youngPeople = people.stream()
+                .filter(person -> person.getAge() <= 18)
+                .limit(10)
+                .collect(Collectors.toList());
+        youngPeople.forEach(System.out::println);
     }
 }
